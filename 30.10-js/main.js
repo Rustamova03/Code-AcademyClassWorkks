b9f44c2e89996898a25ad7e9342b54435ff0c9cf
Rustamova Briliant <briliantrustmova@gmail.com>
2023-10-30 13:45:27 +0400

    js

---
+++ 30.10-js/main.js
@@ -0,0 +1,18 @@
+
+        let firstName="Briliant";
+        let lastName="Rustamova";
+
+
+        let fullName;
+
+        fullName = firstName + " " + lastName;
+
+        console.log("full name: ",fullName);
+
+        
+        let contryName="Azerbaijan";
+        let cityName="Ganja";
+        let jobName="student";
+        let age="21"
+        let personalInfo = fullName + ". I am a " + jobName + " . I live in  " + cityName + ".I'm from "+contryName +".I'm " +age +" years old."
+        console.log(personalInfo)
